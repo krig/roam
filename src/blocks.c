ce90a182ea368af5eaa11557fe97373fa50f5604
Kristoffer Grönlund <krig@koru.se>
2014-10-11 03:12:36 +0200

    too much fun

--- src/blocks.c
+++ src/blocks.c
@@ -12,10 +12,18 @@ struct blockinfo_t blockinfo[] = {
 	{ .name = "stone", .img = { IMG_LIGHT_STONE } },
 	{ .name = "clay", .img = { IMG_CLAY } },
 	{ .name = "snow", .img = { IMG_SNOW } },
+	{ .name = "mint", .img = { IMG_MINT } },
 	{ .name = "red leaves", .img = { IMG_RED_LEAVES } },
 	{ .name = "pig", .img = { IMG_PIGBACK, IMG_PIG_SKIN, IMG_PIGBACK, IMG_PIGBACK, IMG_PIG_HEAD, IMG_PIG_SKIN } },
 	{ .name = "meat", .img = { IMG_MEAT_TOP, IMG_MEAT_TOP, IMG_MEAT_SIDE } },
 	{ .name = "torch", .img = { IMG_TORCH_TOP, IMG_SWORD_HILT, IMG_TORCH_SIDE } },
+	{ .name = "melon", .img = { IMG_MELON_CUT, IMG_MELON_SKIN, IMG_MELON_SIDE } },
+	{ .name = "tree", .img = { IMG_TREE_CUT, IMG_TREE_CUT, IMG_TREE } },
+	{ .name = "birch", .img = { IMG_BIRCH_CUT, IMG_BIRCH_CUT, IMG_BIRCH_TREE } },
+	{ .name = "oak", .img = { IMG_OAK_CUT, IMG_OAK_CUT, IMG_OAK_TREE } },
+	{ .name = "palm", .img = { IMG_PALM_CUT, IMG_PALM_CUT, IMG_PALM_TREE } },
+	{ .name = "snow grass 1", .img = { IMG_SNOW, IMG_WETGRASS, IMG_GRASS_SNOW_1 } },
+	{ .name = "snow grass 2", .img = { IMG_SNOW, IMG_WETGRASS, IMG_GRASS_SNOW_2 } },
 };
 
 void initBlockInfo() {
